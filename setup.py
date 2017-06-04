3f044c56ff3ac6b06024322d781cdb6135c2556d
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-06-04 13:39:16 +0530

    init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from rlagents import __version__
+__version__ = list(map(str, __version__))
+
+setup(name='rlagents',
+      version='.'.join(__version__),
+      description='Reinforcement Learning Agents',
+      url='http://github.com/theSage21/rlagents',
+      author='Arjoonn Sharma',
+      author_email='arjoonn.94@gmail.com',
+      license='MIT',
+      packages=['rlagents'],
+      entry_points={'console_scripts': ['rlagents=rlagents.cli:main']},
+      keywords=['rlagents', 'reinforcement', 'learning'],
+      zip_safe=False)
